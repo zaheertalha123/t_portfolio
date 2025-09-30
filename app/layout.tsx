3c397e820dc8da58b7d79f0bbc5eaa3599406400
Muhammad Talha <zaheertalha123@gmail.com>
2025-09-30 11:16:52 -0700

    Deploy Improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 // import { ScrollProgressIndicator } from "@/components/scroll-progress-indicator"
 import { AnimationProvider } from "@/contexts/animation-context";
 import { getMetaInfo } from "@/lib/data";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
